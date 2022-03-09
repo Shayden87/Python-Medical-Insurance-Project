866b2c4aed57d4af535c0a57ef0672e1331e8b78
Spencer Hayden <75949516+Shayden87@users.noreply.github.com>
2022-03-09 10:31:21 -0800

    added analyze_smoker and analyze_bmi functions

--- Medical_Insurance_Project.py
+++ Medical_Insurance_Project.py
@@ -3,44 +3,42 @@
 # Project: Medical Insurance Project
 # Company: Code Academy 
 # Date: 03/08/2022
-# Version: v1.2
+# Version: v1.3
 # Description: Basic Python project to highlight utilization of
 # Python Syntax by creating formula that estimates a person's yearly
 # insurance cost based on certain variables/criteria. Improved upon 
-# v1.0 as a function to calculate insurance cost was added.
+# v1.20 as a functions to provide more information to smokers and 
+# those with higher BMI was added.
 #####################################################################
 
-# Create calculate_insurance_cost() function below: 
-def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
+# Function to provide user more info on lowering cost with regards to smoking status
+def analyze_smoker(smoker_status):
+  if smoker_status == 1:
+    print("To lower your cost, you should consider quitting smoking.")
+  else:
+    print("Smoking is not an issue for you.")
+
+# Function to provide user more info on lowering cost with regards to BMI value
+def analyze_bmi(bmi_value):
+  if bmi_value > 30:
+    print("Your BMI is in the obese range. To lower your cost, you should significantly lower your BMI.")
+  elif bmi_value >= 25 and bmi_value <= 30:
+    print("Your BMI is in the overweight range. To lower your cost, you should lower your BMI.")
+  elif bmi_value >= 18.5 and bmi_value < 25:
+    print("Your BMI is in a healthy range.")
+  elif bmi_value < 18.5: 
+    print("Your BMI is in the underweight range. Increasing your BMI will not help lower your cost, but it will help improve your health.")
+
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
   estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-  print("The estimated insurance cost for " + name + " is " + str(estimated_cost) + " dollars.")
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+  analyze_smoker(smoker)
+  analyze_bmi(bmi)
   return estimated_cost
+ 
+# Estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(name = 'Keanu', age = 29, sex = 1, bmi = 26.2, num_of_children = 3, smoker = 1)
 
-# Initial variables for Maria 
-age = 28
-sex = 0  
-bmi = 26.2
-num_of_children = 3
-smoker = 0  
-name = "Maria"
-# Estimate Maria's insurance cost
-maria_insurance_cost = calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name)
-
-# Initial variables for Omar
-age = 35
-sex = 1 
-bmi = 22.2
-num_of_children = 0
-smoker = 1  
-name = "Omar"
-# Estimate Omar's insurance cost 
-omar_insurance_cost = calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name)
-
-age = 34
-sex = 1 
-bmi = 16.2
-num_of_children = 0
-smoker = 1  
-name = "Spencer"
-# Estimate my insurance cost 
-my_insurance_cost = calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name)
+# Estimate my insurance cost
+my_insurance_cost = estimate_insurance_cost(name = 'Spencer', age = 34, sex = 1, bmi = 18.2, num_of_children = 0, smoker = 1)
